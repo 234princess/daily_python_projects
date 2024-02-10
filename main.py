8c94df7165fb90120123324649ae885e17a2acff
234princess <150721600+234princess@users.noreply.github.com>
2024-02-10 12:59:27 +0000

    building turtle crossing game

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
-#import practice_module
-#print(practice_module.number)
+import time
 
-#from practice_module import number
-#print(number)
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
-#import turtle
-#timi = turtle.Turtle()
 
-#from turtle import Turtle,Screen
-#timy = Turtle()
-#print(timy)
-#timy.shape("turtle")
-#timy.color('red')
-#timy.forward(100)
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.listen()
 
-#screen = Screen()
-#print(screen.canvheight)
-#print(screen.exitonclick())
 
-from prettytable import PrettyTable
-table = PrettyTable()
+turtle_player = Player()
+cars = CarManager()
+scoreboard = Scoreboard()
 
-table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
+screen.onkey(fun=turtle_player.move_up, key="Up")
 
-table.align = "l"
-print (table)
+ 
+#TODO:Create a scoreboard that keeps track of which level the user is on. Every time the turtle player does a successful crossing, the level should increase. When the turtle hits a car, GAME OVER should be displayed in the centre. If you get stuck, check the video walkthrough in Step 7.
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    cars.create_car()
+    cars.move_cars()
+
+    #TODO:Detect when the turtle player collides with a car and stop the game if this happens.
+    for car in cars.all_cars:
+        if car.distance(turtle_player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    #TODO:Detect when the turtle player has reached the top edge of the screen (i.e., reached the FINISH_LINE_Y).      
+    if turtle_player.at_finish_line():
+        turtle_player.go_to_start()
+        cars.level_up()
+        scoreboard.increase_level()
+    
+
+
+screen.exitonclick()
