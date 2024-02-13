d64afe1062c86f51a094f0291bd589ec1eb15060
234princess <150721600+234princess@users.noreply.github.com>
2024-02-13 23:58:58 +0100

    building a state guessing game (USA)

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-PLACEHOLDER = "[name]"
+import turtle
+import pandas
 
-with open("./Input/Names/invited_names.txt", mode="r") as names:
-    list_of_names= names.readlines()
-    #print(list_of_names)
-    
+data = pandas.read_csv("50_states.csv")
+state_list = data.state.to_list()
 
-with open("./Input/Letters/starting_letter.txt", mode = "r") as letter:
-    letter_template = letter.read()
-    for name in list_of_names:
-        stripped_name = name.strip()
-        new_letter = letter_template.replace(PLACEHOLDER,stripped_name)
-        #print(new_letter)
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
 
-        with open(f"./Output/ReadytoSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
-            completed_letter.write(new_letter)
-        
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title= f"{len(guessed_states)}/50 : Guess the State", prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in state_list:
+            if state not in guessed_states:
+                missing_states.append(state)
+            new_data = pandas.DataFrame(missing_states)
+            new_data.to_csv("states_to_learn.csv")
+    if answer_state in state_list:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.pu()
 
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
+#def get_mouse_click_coor(x,y):
+    #print(x,y)
+#turtle.onscreenclick(get_mouse_click_coor)
+#turtle.mainloop()
+#screen.exitonclick()
