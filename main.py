3df6eaa8d969268c384ec222d50b84527b9a971d
234princess <150721600+234princess@users.noreply.github.com>
2024-02-13 08:37:06 +0100

    building a mail merger

--- main.py
+++ main.py
@@ -1,46 +1,19 @@
-import time
+PLACEHOLDER = "[name]"
 
-from turtle import Screen
-from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
-
-
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
-screen.listen()
-
-
-turtle_player = Player()
-cars = CarManager()
-scoreboard = Scoreboard()
-
-screen.onkey(fun=turtle_player.move_up, key="Up")
-
- 
-#TODO:Create a scoreboard that keeps track of which level the user is on. Every time the turtle player does a successful crossing, the level should increase. When the turtle hits a car, GAME OVER should be displayed in the centre. If you get stuck, check the video walkthrough in Step 7.
-
-game_is_on = True
-while game_is_on:
-    time.sleep(0.1)
-    screen.update()
-
-    cars.create_car()
-    cars.move_cars()
-
-    #TODO:Detect when the turtle player collides with a car and stop the game if this happens.
-    for car in cars.all_cars:
-        if car.distance(turtle_player) < 20:
-            game_is_on = False
-            scoreboard.game_over()
-
-    #TODO:Detect when the turtle player has reached the top edge of the screen (i.e., reached the FINISH_LINE_Y).      
-    if turtle_player.at_finish_line():
-        turtle_player.go_to_start()
-        cars.level_up()
-        scoreboard.increase_level()
+with open("./Input/Names/invited_names.txt", mode="r") as names:
+    list_of_names= names.readlines()
+    #print(list_of_names)
     
 
+with open("./Input/Letters/starting_letter.txt", mode = "r") as letter:
+    letter_template = letter.read()
+    for name in list_of_names:
+        stripped_name = name.strip()
+        new_letter = letter_template.replace(PLACEHOLDER,stripped_name)
+        #print(new_letter)
 
-screen.exitonclick()
+        with open(f"./Output/ReadytoSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
+        
+
+
