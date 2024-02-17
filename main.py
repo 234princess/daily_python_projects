6bc49c44206551a4b87d7a9e15fc779fee68b9d7
234princess <150721600+234princess@users.noreply.github.com>
2024-02-17 14:38:32 +0100

    building a miles to km converter

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-import pandas
+from tkinter import *
 
-data = pandas.read_csv("data.csv")
-phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows()}
 
-user_input = input("Enter a word : ").upper()
-output_list = [phonetic_dict[letter] for letter in user_input]
-print(output_list)
+def calculate():
+    miles = entry.get()
+    kilometer = round(float(miles) * 1.6)
+    return solution.config(text=f'{kilometer}')
+
+
+window = Tk()
+window.title("Miles to Km Converter")
+# window.minsize(width=250, height=125)
+window.config(padx=20, pady=20)
+
+entry = Entry(width=5)
+entry.grid(row=0, column=1)
+
+miles_label = Label(text="Miles")
+miles_label.grid(row=0, column=2)
+
+equals = Label(text="is equals to")
+equals.grid(row=1, column=0)
+
+solution = Label(text="0")
+solution.grid(row=1, column=1)
+
+km_label = Label(text="Km")
+km_label.grid(row=1, column=2)
+
+
+button = Button(text="Calculate", command=calculate)
+button.grid(column=1, row=3)
+
+
+window.mainloop()
+
