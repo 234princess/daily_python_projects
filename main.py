cc6f01197657c4d98d964d45b75bc0370d9d6166
234princess <150721600+234princess@users.noreply.github.com>
2024-02-20 16:21:58 +0100

    updating the password manager with a search button and json file

--- main.py
+++ main.py
@@ -3,9 +3,29 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import json
+# ---------------------------- SEARCH WEBSITE ------------------------------- #
 
-# Password Generator Project
+
+def search():
+    website = website_entry.get().lower()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email : {email}\nPassword: {password}")
+
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
 def generate_password():
@@ -36,6 +56,13 @@ def check_data():
     user_email = email_entry.get()
     user_password = password_entry.get()
 
+    new_data = {
+        user_website: {
+            "email": user_email,
+            "password": user_password
+        }
+    }
+
     if len(user_website) > 0 or len(user_password) > 0:
 
         is_ok = messagebox.askokcancel(title=user_website,
@@ -43,14 +70,30 @@ def check_data():
                                                f"\nPassword: {user_password} \nIs this okay to save? ")
 
         if is_ok:
-            with open("data.txt", "a") as data:
-                data.write(f"{user_website} | {user_email} | {user_password}\n")
+            try:
+                with open("data.json", "r") as data_file:
+                    # data.write(f"{user_website} | {user_email} | {user_password}\n")
+                    # Reading old data
+                    data = json.load(data_file)
 
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
+
+            else:
+                # Updating old data with new data
+                data.update(new_data)
+
+                with open("data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+
+            finally:
                 website_entry.delete(0, END)
-                email_entry.delete(0, END)
+                # email_entry.delete(0, END)
                 password_entry.delete(0, END)
-    else:
-        messagebox.showwarning(title="No input received", message="Please do not leave any field empty!")
+    # else:
+        # messagebox.showwarning(title="No input received", message="Please do not leave any field empty!")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -69,8 +112,8 @@ canvas.grid(row=0, column=1)
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1, columnspan=1)
 website_entry.focus()
 
 email_label = Label(text="Email/Username:")
@@ -82,11 +125,14 @@ email_entry.insert(0, "mcodes@gmail.com")
 
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
-password_entry = Entry(width=17)
+password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 password_button = Button(text="Generate Password", command=generate_password)
 password_button.grid(row=3, column=2)
+
+search_button = Button(text="Search", width=13, command=search)
+search_button.grid(row=1, column=2)
 
 add_button = Button(text="Add", width=30, command=check_data)
 add_button.grid(row=4, column=1, columnspan=2)
